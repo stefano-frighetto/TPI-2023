2fb43328038d37c8b20d7b4e0885ecaae956ce4d
Stéfano Frighetto <frighettostefano@gmail.com>
2023-06-19 11:49:50 -0300

    Declaración de funciones de la clase Vista. Creación de ControladorAgenda

---
+++ Controllers/ControladorAgenda.py
@@ -0,0 +1,5 @@
+from Models.Agenda import Agenda
+
+class ControladorAgenda:
+    def __init__(self):
+        self.agenda = Agenda()
---
+++ Views/VistaAgenda.py
@@ -0,0 +1,87 @@
+import datetime
+
+class VistaAgenda:
+    def validar_entero(self, lim_inf, lim_sup):
+        while True:
+            try:
+                choice = int(input(f'Ingrese una opción entre {lim_inf} y {lim_sup}\n'))
+            except ValueError:
+                print('Opción inválida. Intente nuevamente:')
+                continue
+            else:
+                if choice < lim_inf or choice > lim_sup:
+                    print('Opción inválida. Intente de nuevo:')
+                    continue
+                else:
+                    return choice
+
+    def mostrar_menu_principal(self):
+        print('"""""""""""""""""""""""""""""""""""""""""""""""""""""')
+        print("Bienvenido al sistema de reservas de SocialEvent S.A.")
+        print('"""""""""""""""""""""""""""""""""""""""""""""""""""""')
+        print("1. Realizar una nueva reserva")
+        print('2. Abonar seña pendiente')        
+        print('3. Agregar o quitar servicios')
+        print("4. Cancelar una reserva")
+        print("5. Salir")
+    
+    def solicitar_fecha(self):
+        print('¿Qué fecha desea reservar para su evento')
+        print('Ingrese el día')
+        day = self.validar_entero(1,31)
+        print('Ingrese el mes')
+        month = self.validar_entero(1,12)
+        print('Ingrese el año')
+        year = self.validar_entero(2023,2026)
+        return datetime.date(year, month, day)
+    
+    def mostrar_fecha_propuesta(self, date):
+        print('La fecha solicitada no está disponible')
+        print('Se ofrece la siguiente fecha a modo de reemplazo:')
+        print(f'{date.day}/{date.month}/{date.year}')
+        print('¿Desea reservar ésta fecha?')
+        print('1- Sí')
+        print('2- No (Mostrar otra opción)')
+        print('3- No (Cancelar solicitud de evento)')
+        rta = self.validar_entero(1,3)
+        #No estoy para nada seguro de si funciona manejar estos return
+        if rta == 1:
+            return True
+        elif rta == 2:
+            return date + datetime.timedelta(days=1)
+        else:
+            return False
+
+    def solicitar_datos_cliente(self):
+        print('Ingrese el nombre del cliente:')
+        nombre = input()
+        print('Ingrese el DNI del cliente')
+        dni = input()
+    
+    #La puedo usar para elegir evento también. ver si se puede hacer genérica
+    def solicitar_servicio(self, lista_de_servicios):
+        print('Seleccione un servicio de la lista:')
+        for i, servicio in lista_de_servicios:
+            print(f'{i+1}- {servicio}')
+        rta = self.validar_entero(1,len(lista_de_servicios))
+        return lista_de_servicios[rta-1]
+
+    def solicitar_senia(self, costo_senia):
+        print(f'El monto de la seña para reservar este evento es ${costo_senia}')
+        print('¿Desea abonarla ahora?')
+        print('1- Sí')
+        print('2- No')
+        rta = self.validar_entero(1,2)
+        if rta == 1:
+            print('Seña abonada')
+            return True
+        else:
+            print('Su evento fue registrado, pero la reserva no será registrada hasta que abone la seña correspondiente')
+            return False
+    
+    def mostrar_cancelacion(self, evento):
+        fecha_actual = datetime.date.today()
+        if evento.fecha_evento - fecha_actual > 15:
+            print(f'Su cancelación fue realizada con anticipación, por lo que se le reintegrarán ${evento.importe_recibido_senia * 0.3}')
+        else:
+            print('Su cancelación ha sido realizada con una anticipación menor a 15 días, por lo que no corresponde reintegro.')
